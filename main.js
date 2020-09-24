2edce9c54f8937d3d5139818498e38d963c88ff9
dongxiaoxing <dongxiaoxing0@gmail.com>
2020-09-24 23:31:19 +0800

    添加设备检测（特性检测）

--- main.js
+++ main.js
@@ -17,51 +17,85 @@ var lastPoint = {
     x: undefined,
     y: undefined,
 }
-context.fillStyle = 'black';
+
+
+
+if (document.body.touchstart !== undefined) {
+    canvas.onmousedown = function (event) {
+        enabled = !enabled;
+        var x = event.clientX;
+        var y = event.clientY;
+        drawCircle(x, y);
+        lastPoint.x = x;
+        lastPoint.y = y;
+    }
+    
+    
+    canvas.onmousemove = function (event) {
+        if (enabled) {
+            var x = event.clientX;
+            var y = event.clientY;
+            if (eraserEnabled) {
+                context.clearRect(x-10,y-10,20,20);
+            }else{
+                drawCircle(x, y);
+                context.lineWidth = 10;
+                context.beginPath();
+                context.moveTo(x,y);
+                context.lineTo(lastPoint.x, lastPoint.y);
+                context.closePath();
+                context.stroke();
+                lastPoint.x = x;
+                lastPoint.y = y;
+            }
+        }
+    }
+    canvas.onmouseup = function () {
+        enabled = !enabled;
+    }
+}else{
+        canvas.ontouchstart = function (event) {
+            enabled = !enabled;
+            var x = event.touches[0].clientX;
+            var y = event.touches[0].clientY;
+            drawCircle(x, y);
+            lastPoint.x = x;
+            lastPoint.y = y;
+        }
+        
+        
+        canvas.ontouchmove = function (event) {
+            if (enabled) {
+                var x = event.touches[0].clientX;
+                var y = event.touches[0].clientY;
+                if (eraserEnabled) {
+                    context.clearRect(x-10,y-10,20,20);
+                }else{
+                    drawCircle(x, y);
+                    context.lineWidth = 10;
+                    context.beginPath();
+                    context.moveTo(x,y);
+                    context.lineTo(lastPoint.x, lastPoint.y);
+                    context.closePath();
+                    context.stroke();
+                    lastPoint.x = x;
+                    lastPoint.y = y;
+                }
+            }
+        }
+        canvas.ontouchend = function () {
+            enabled = !enabled;    
+    }
+}
+
+
 function drawCircle(x, y) {
     context.beginPath();
     context.arc(x, y, 5, 0, Math.PI * 2);
     context.fill();
 };
 
-canvas.onmousedown = function (event) {
-    enabled = !enabled;
-    var x = event.clientX;
-    var y = event.clientY;
-    drawCircle(x, y);
-    lastPoint.x = x;
-    lastPoint.y = y;
-}
 
-
-canvas.onmousemove = function (event) {
-    if (enabled) {
-        var x = event.clientX;
-        var y = event.clientY;
-        if (eraserEnabled) {
-            context.clearRect(x-10,y-10,20,20);
-        }else{
-            drawCircle(x, y);
-            context.lineWidth = 10;
-            context.beginPath();
-            context.moveTo(x,y);
-            context.lineTo(lastPoint.x, lastPoint.y);
-            context.closePath();
-            context.stroke();
-            lastPoint.x = x;
-            lastPoint.y = y;
-        }
-
-
-    }
-
-
-
-}
-
-canvas.onmouseup = function () {
-    enabled = !enabled;
-}
 
 var eraser = document.getElementById('eraser');
 eraser.onclick = function () {
